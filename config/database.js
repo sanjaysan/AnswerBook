5e755c589a059a7e430a88ba373179d9f5fc262a
sanjaysan <sanjaysan01@gmail.com>
2018-01-12 03:40:43 -0600

    Changed database config to heroku database

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: 'localhost',
+  host: 'ec2-184-73-175-95.compute-1.amazonaws.com',
   port: 5432,
-  database: 'answerbook',
-  user: 'postgres',
-  password: 'postgres',
+  database: 'dcga6cgsv87r25',
+  user: 'ptshrkhlcmuxxb',
+  password: '03aa9e21a5ac613f6c0936325019c89bd5b007968282c9ad7ccfbb6d4f4ba3cb',
   secret: 'newstartup'
 };
