081e966838eea1f76b458aed3dedf48db37409c7
sanjaysan <sanjaysan01@gmail.com>
2018-01-12 22:56:20 -0600

    Made changes to navbar

--- answerbookui/src/components/navbar/navbar.js
+++ answerbookui/src/components/navbar/navbar.js
@@ -19,6 +19,11 @@ class Navbar extends Component {
       <Notifications/>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <Link className="navbar-brand" to="/">AnswerBook</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarColor01" aria-controls="navbarColor01"
+                aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"/>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
