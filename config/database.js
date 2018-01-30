dd8f6cc9842281aa06d646ae1bb543631f58e64e
sanjaysan <sanjaysan01@gmail.com>
2018-01-30 02:14:11 -0600

    Added both local and remote DB configurations to the database.js file

--- config/database.js
+++ config/database.js
@@ -1,8 +1,19 @@
+// Local Postgres DB
+// module.exports = {
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'answerbook',
+//   user: 'postgres',
+//   password: 'postgres',
+//   secret: 'newstartup'
+// };
+
+// Remote Postgres DB
 module.exports = {
-  host: 'localhost',
+  host: 'baasu.db.elephantsql.com',
   port: 5432,
-  database: 'answerbook',
-  user: 'postgres',
-  password: 'postgres',
+  database: 'uvhexsbb',
+  user: 'uvhexsbb',
+  password: 'pz8CarFn27leORQBwt_fqRMgxxf6RSaz',
   secret: 'newstartup'
 };
