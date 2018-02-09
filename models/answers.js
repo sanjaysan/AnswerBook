93e5251c905f6e44a24404da5fc39bd61323d9c9
sanjaysan <sanjaysan01@gmail.com>
2018-02-08 23:56:44 -0600

    Removing node_modules

--- models/answers.js
+++ models/answers.js
@@ -1,43 +1,36 @@
 module.exports = function (sequelize, DataTypes) {
-    const answers = sequelize.define('answers', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-
-        upvotes: {
-            type: DataTypes.INTEGER,
-            min: 0,
-            allowNull: true
-        },
-
-        body: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-
-        accepted: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-
-        ready_list: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
-    });
-
-    answers.addAnswer = function (answer, callback) {
-
-        answers.create(answer).then(function (answer) {
-
-            return callback(null, answer)
-        }).catch(function (err) {
-
-            return callback(err, null)
-        })
+  const answers = sequelize.define('answers', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      min: 0,
+      allowNull: true
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    accepted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    ready_list: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
+  });
 
-    return answers;
+  answers.addAnswer = function (answer, callback) {
+    answers.create(answer).then(function (answer) {
+      return callback(null, answer)
+    }).catch(function (err) {
+      return callback(err, null)
+    })
+  };
+
+  return answers;
 };
