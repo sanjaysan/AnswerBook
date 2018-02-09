f3426ba3b6ee495bc63fdb03059af48215d55be2
Varun Ramesh <varun.ceg.95@gmail.com>
2018-02-08 18:35:51 -0600

    abc

--- models/answers.js
+++ models/answers.js
@@ -1,43 +1,43 @@
 module.exports = function (sequelize, DataTypes) {
-  const answers =  sequelize.define('answers', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
+    const answers = sequelize.define('answers', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
 
-    upvotes: {
-      type: DataTypes.INTEGER,
-      min: 0,
-      allowNull: true
-    },
+        upvotes: {
+            type: DataTypes.INTEGER,
+            min: 0,
+            allowNull: true
+        },
 
-      body: {
-        type: DataTypes.TEXT,
-          allowNull: false
-      },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
 
-    accepted: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
+        accepted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
 
-    ready_list: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
+        ready_list: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+
+    answers.addAnswer = function (answer) {
+
+        answers.create(answer).then(function (answer) {
+
+            return callback(null, answer)
+        }).catch(function (err) {
+
+            return callback(err, null)
+        })
     }
-  });
-
-  answers.addAnswer = function (answer) {
-
-      answers.create(answer).then(function (answer) {
-
-          return callback(null, answer)
-      }).catch(function(err) {
-
-          return callback(err,null)
-      })
-  }
 
 
 };
